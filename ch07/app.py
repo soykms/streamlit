f94df8b92d3665313a9e3e4708786dc2ba0146b4
soykms <hulabarms@gmail.com>
2024-08-30 21:07:19 +0900

    commit msg

---
+++ ch07/app.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+
+import sktime 
+import pmdarima
+import pandas as pd 
+import matplotlib
+import seaborn
+import plotly
+import sklearn
+import streamlit
+import yfinance
+import lightgbm
+import prophet
+import statsmodels
+import geopandas
+import folium
+import xmltodict
+import millify
+import pingouin
+
+def main():
+    print(sktime.__version__)
+    print(pmdarima.__version__)
+    print(pd.__version__)
+    print(matplotlib.__version__)
+    print(seaborn.__version__)
+    print(plotly.__version__)
+    print(sklearn.__version__)
+    print(streamlit.__version__)
+    print(yfinance.__version__)
+    print(lightgbm.__version__)
+    print(prophet.__version__)
+    print(statsmodels.__version__)
+    print(geopandas.__version__)
+    print(folium.__version__)
+    print(xmltodict.__version__)
+    print(millify.__version__)
+    print(pingouin.__version__)
+
+if __name__ == "__main__":
+    main()
